b3108a899a976ab6ff2cc96644d81debbc876a3f
yabushan <392425349@qq.com>
2016-10-19 22:01:38 +0800

    文件上传与下载工具类，excel系统学习开始

---
+++ leap-user-guid/src/main/java/leap/demo/util/zipUtil2/ExtendNameFilter.java
@@ -0,0 +1,13 @@
+package leap.demo.util.zipUtil2;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class ExtendNameFilter implements FileFilter {
+     public boolean accept(File pathname) {
+         if (pathname.getName().toLowerCase().endsWith(".txt".toLowerCase())
+        		 ||pathname.isDirectory())//判断文件的扩展名是否符合指定扩展名
+             return true;
+         return false;
+     }
+}
